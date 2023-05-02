d6920b728c584e857f4973c4298851ec4bc3f98b
manobalaban <mano.fabian@codecool.com>
2023-05-02 13:00:07 +0200

    refactoring code, add eslint

---
+++ README.md
@@ -0,0 +1,27 @@
+### Once You download:
+
+At the terminal, run:
+ `npm install` / `npm i`.
+
+### Mandatory:
+
+- 4x4 board
+- minimum 2 1x1 ships
+- they cannot be next to each other, at most diagonally
+- the game has two phases
+    - placement phase: in this phase you have to place the ships by clicking on the fields
+    - shooting phase: n this phase we click to shoot at the ships in the other field
+
+- AI shoot shoots at random places on our board, once the ships have sunk, it's game over
+- game selection - found in data.js file here you can see where the AI ​​places the ships (function selectGame belongs to it)
+- You have to set the AI ​​to see if it hit the ship
+- Use as few variables as possible
+- you can communicate externally via message
+
+### OPT:
+
+- it must be adjusted so that larger ships can be placed on 5x5 fields
+- left click: horizontal
+- right click: vertical
+- data must be validated
+- Smart AI
--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-let board = [["w","w","",""],["","","","0"],["q","","",""],["","","",""]];
-const board2 = [["w","w","","",""],["","","","0", ""],["q","","","", ""],["","0","0","", ""], ["", "", "", "", ""]];
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 
+/**
+ * This function called when You chose the game mode.
+ * Gives You the data about what kind of game the AI would like to play, and where it places its ships.
+ * @param {String} data the selected game mode, You have to parse it to use.
+ */
 function selectGame(data) {
-  displayMessage(data, "black");
+  displayMessage(data, 'black'); // Just an example to see how the data looks like, You may delete this line.
 }
 
+/**
+ * Called every time, when the player clicks on a cell.
+ * @param {Object} data the clicked cells data.
+ */
 function handleClick(data) {
-  displayMessage(data.x + data.y + data.clickType);
+  displayMessage(data.x + data.y + data.clickType); // Just an example to see how the data looks like, You may delete this line.
 }
 
+/**
+ * Called when the player clicks on the reset game button.
+ */
 function resetGame() {
-  board = [];
-  for(let i = 0; i < 10; i++) {
-    board.push([])
-    for(let j = 0; j < 10; j++) {
-      board[i].push("<button></button>");
+  // You can dete the whole body of this function (it is just an example), and implement Your solution.
+  const board = [];
+  for (let i = 0; i < 10; i++) {
+    board.push([]);
+    for (let j = 0; j < 10; j++) {
+      board[i].push('');
     }
   }
-  displayBoard({boardnumber: 1,board: board});
-  displayBoard({boardnumber: 2,board: board});
+  displayBoard({ boardNumber: 1, board: board });
+  displayBoard({ boardNumber: 2, board: board });
 }
 
+/**
+ * Called when the player clicks on the AI shoot button. Gives You random genarated coordinates.
+ * You may leave the data later, when You implements smarter AI.
+ * @param {Object} data Random generated coordinates, where the AI would like to shoot.
+ */
 function aiShoot(data) {
-  
-  console.log(data);
+  displayMessage(data.x + data.y);
 }
 
-displayBoard({boardnumber: 1,board: board});
-displayBoard({boardnumber: 2,board: board2});
-displayMessage("message", "green");
-displayTextMessage("text message", "red");
+
+/*Example to show how the three callable function looks like in action.
+The displayBoard function requeres an object as argument and it should have two property:
+boardNumber with 1 (left) or 2 (right), to decide where would You like to display,
+and board which sould be a nested array, to display.
+displayMessage and displayTextMessage are functions to display messages:
+requires two argument: first is a string to display, and the second is a color (can be text, rgb, rgba, hex color)
+*/
+displayBoard({ boardnumber: 1, board:
+  [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']],
+});
+displayBoard({ boardnumber: 2, board:
+  [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']],
+});
+displayMessage('message', 'green');
+displayTextMessage('text message', 'red');
